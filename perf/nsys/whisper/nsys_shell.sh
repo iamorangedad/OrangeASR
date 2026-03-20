bb319bdb3367aba7e801a87ae0e6ef1d07ce01be
zhumj <allenexecutivezhu@gmail.com>
2026-03-20 16:28:25 -0700

    feat: remove the module of profile

--- perf/nsys/whisper/nsys_shell.sh
+++
@@ -1,8 +0,0 @@
-sudo jetson_clocks
-
-sudo nsys profile \
-  -t nvtx,cuda,osrt,cudnn,cublas \
-  -s cpu \
-  -o whisper_profile_result \
-  --force-overwrite true \
-  python3 nsys_profile_whisper.py
